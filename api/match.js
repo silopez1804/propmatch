7d4740cbbe38a46506b32458429d04305534f74b
silopez1804 <silopez1804@gmail.com>
2026-05-11 17:16:29 -0600

    Update match.js

--- api/match.js
+++ api/match.js
@@ -1,4 +1,5 @@
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
+
   return res.status(200).json({
     encontrados: 1,
     matches: [
@@ -6,11 +7,9 @@ export default async function handler(req, res) {
         "id público (EB)": "TEST123",
         "título de propiedad": "Casa prueba en Polanco",
         "precio de venta": "10000000",
-        "colonia/zona/barrio": "Polanco",
-        "recámaras": 3,
-        "baños": 2,
-        "estacionamientos": 2
+        "colonia/zona/barrio": "Polanco"
       }
     ]
   });
-}
+
+};
